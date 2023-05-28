df8e881ff1c242afe58241ef34d8d1e30a30037e
jrencelj <86268298+jrencelj@users.noreply.github.com>
2023-05-28 20:26:42 +0200

    Add files via upload

---
+++ BloomovFilter/Program.cs
@@ -0,0 +1,24 @@
+﻿using BloomFilter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bloomovfilter
+{
+    internal class Program
+    {
+        private static IBloomFilter bloomovFilter = FilterBuilder.Build(25, 0.1);
+        static void Main(string[] args)
+        {
+            string abeceda = "abcčdefghijklmnoprsštuvzž";
+            foreach (char crka in abeceda)
+            {
+                bloomovFilter.Add(crka);
+            }
+            Console.WriteLine(bloomovFilter.Contains(' '));
+            Console.ReadKey();
+        }
+    }
+}

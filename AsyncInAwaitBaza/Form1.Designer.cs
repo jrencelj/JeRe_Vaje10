f111c409071fe5d7f2b65b1acdd81c5a8e54633e
jrencelj <86268298+jrencelj@users.noreply.github.com>
2023-05-28 20:19:48 +0200

    Add files via upload

---
+++ AsyncInAwaitBaza/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace AsyncInAwaitBaza
+{
+    partial class okno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.isci_button = new System.Windows.Forms.Button();
+            this.rezultat = new System.Windows.Forms.TextBox();
+            this.podrocjeComboBox = new System.Windows.Forms.ComboBox();
+            this.nobelLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // isci_button
+            // 
+            this.isci_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.isci_button.Location = new System.Drawing.Point(40, 331);
+            this.isci_button.Name = "isci_button";
+            this.isci_button.Size = new System.Drawing.Size(150, 50);
+            this.isci_button.TabIndex = 0;
+            this.isci_button.Text = "Išči";
+            this.isci_button.UseVisualStyleBackColor = true;
+            this.isci_button.Click += new System.EventHandler(this.Isci);
+            // 
+            // rezultat
+            // 
+            this.rezultat.Location = new System.Drawing.Point(312, 12);
+            this.rezultat.Multiline = true;
+            this.rezultat.Name = "rezultat";
+            this.rezultat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.rezultat.Size = new System.Drawing.Size(476, 426);
+            this.rezultat.TabIndex = 1;
+            // 
+            // podrocjeComboBox
+            // 
+            this.podrocjeComboBox.FormattingEnabled = true;
+            this.podrocjeComboBox.Location = new System.Drawing.Point(40, 194);
+            this.podrocjeComboBox.Name = "podrocjeComboBox";
+            this.podrocjeComboBox.Size = new System.Drawing.Size(150, 28);
+            this.podrocjeComboBox.TabIndex = 2;
+            // 
+            // nobelLabel
+            // 
+            this.nobelLabel.AutoSize = true;
+            this.nobelLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nobelLabel.Location = new System.Drawing.Point(53, 49);
+            this.nobelLabel.Name = "nobelLabel";
+            this.nobelLabel.Size = new System.Drawing.Size(106, 37);
+            this.nobelLabel.TabIndex = 3;
+            this.nobelLabel.Text = "Nobel";
+            // 
+            // okno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nobelLabel);
+            this.Controls.Add(this.podrocjeComboBox);
+            this.Controls.Add(this.rezultat);
+            this.Controls.Add(this.isci_button);
+            this.Name = "okno";
+            this.Text = "Okno";
+            this.Load += new System.EventHandler(this.OknoNalozi);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button isci_button;
+        private System.Windows.Forms.TextBox rezultat;
+        private System.Windows.Forms.ComboBox podrocjeComboBox;
+        private System.Windows.Forms.Label nobelLabel;
+    }
+}
+
---
+++ AsyncInAwaitBaza/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace AsyncInAwaitBaza
+{
+    
+    public partial class okno : Form
+    {
+        private String povezovalniNiz = "Server= baza.fmf.uni-lj.si; User Id= student11; Password= student; Database= nobel2012;";
+        public okno()
+        {
+            InitializeComponent();
+        }
+
+        private async void Isci(object sender, EventArgs e)
+        {
+            rezultat.Text = "";
+            NpgsqlConnection povezava = new NpgsqlConnection(povezovalniNiz);
+            String sqlUkaz = $"Select * FROM Nobel WHERE subject = '{podrocjeComboBox.Text}'";
+            NpgsqlCommand ukaz = new NpgsqlCommand(sqlUkaz, povezava);
+
+            await Task.Delay(5000);
+            await povezava.OpenAsync();
+            NpgsqlDataReader kurzor = await ukaz.ExecuteReaderAsync();
+
+            while (await kurzor.ReadAsync())
+            {
+                string področje = kurzor["subject"].ToString();
+                string leto = kurzor["yr"].ToString();
+                string dobitnik = kurzor["winner"].ToString();
+                string podr = področje;
+                rezultat.AppendText(String.Format("* {0} {1} {2}.", leto, podr, dobitnik));
+                rezultat.AppendText(Environment.NewLine);
+                }
+            }
+
+        private void OknoNalozi(object sender, EventArgs e)
+        {
+            NpgsqlConnection povezava = new NpgsqlConnection(povezovalniNiz);
+            povezava.Open();
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter("SELECT DISTINCT subject FROM nobel", povezava);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            podrocjeComboBox.DisplayMember = "subject";
+            podrocjeComboBox.DataSource = dt;
+            povezava.Close();
+
+        }
+    }
+    }
+
